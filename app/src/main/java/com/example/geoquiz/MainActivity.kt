0c027b0d76997ecb5346193b9bef8a330d8a97f2
FedorByssov <bysov1987@mail.ru>
2023-03-22 12:45:42 +0300

    first chapter

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.geoquiz
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.geoquiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding
+        get() = _binding!!
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.trueButton.setOnClickListener {
+            Toast.makeText(
+                this, R.string.correct_toast, Toast.LENGTH_SHORT
+            ).show()
+
+        }
+        binding.falseButton.setOnClickListener {
+            val toast = Toast
+                .makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT)
+                .show()
+
+
+        }
     }
 }
